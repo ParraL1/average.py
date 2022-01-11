3a0b73c3df6e5833a8ec7ebbdbc9f933fa2c1a6f
ParraL1 <97068405+ParraL1@users.noreply.github.com>
2022-01-11 17:24:12 -0600

    works

--- average.py
+++ average.py
@@ -3,11 +3,11 @@
 # Date: 01/11/2022
 #Description: Average of five numbers
 print("Please enter five numbers.")
-num_1 = float(input())
-num_2 = float(input())
-num_3 = float(input())
-num_4 = float(input())
-num_5 = float(input())
-print("The average of those numbers is:")
-result = (num_1 + num_2 + num_3 + num_4 + num_5) // 5
+num_1 = float (input())
+num_2 = float (input())
+num_3 = float (input())
+num_4 = float (input())
+num_5 = float (input())
+print ("The average of those numbers is:")
+result = ((num_1  + (num_2 + num_3 +  num_4) + num_5) / 5)
 print (result)
