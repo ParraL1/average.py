22795480843b44f9cc302d10dbfc53b907c0cb31
ParraL1 <97068405+ParraL1@users.noreply.github.com>
2022-01-11 18:41:31 -0600

    works!

--- change.py
+++ change.py
@@ -6,9 +6,9 @@ print("Please enter an amount in cents less than a dollar.")
 num_1= float(input())
 print("Your change will be:")
 print("Q:",num_1//25)
-num_1 = num_1%25
+num_1=num_1%25
 print("D:",num_1//10)
-num_1 = num_1%10
+num_1=num_1%10
 print("N:",num_1//5)
-num_1 = num_1%5
+num_1=num_1%5
 print("P:",num_1//1)
