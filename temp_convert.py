c9a018d1722055b6d5d307e6b354a35520c96a8b
ParraL1 <97068405+ParraL1@users.noreply.github.com>
2022-01-11 18:18:53 -0600

    Got it

---
+++ temp_convert.py
@@ -0,0 +1,10 @@
+#Author: Lilliana Parra
+# Github user: ParraL1
+# Date: 01/11/2022
+# Description: Converts a temperature in Celsius to Fahrenheit
+
+print("Please enter a Celsius temperature.")
+num_1 = float(input())
+print("The equivalent Fahrenheit temperature is:")
+result = (num_1 * (9 / 5)) + 32
+print(result)
